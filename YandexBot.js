42f521134afdf133e1b98c1ef85f2ca51aee69d7
Олеся <89357397+Olesya2121@users.noreply.github.com>
2021-09-06 04:18:02 +0300

    Окончательная версия
    1. Бот дополнен новым функционалом. Может работать с неограниченным количеством сайтов и ключевых фраз.
    2. Может сохранять промежуточные значения в куки.

--- YandexBot.js
+++ YandexBot.js
@@ -5,12 +5,20 @@
 // @description  Bot
 // @author       OlesyaBatsanova
 // @match        https://ya.ru/*
-// @match        https://yandex.ru/*
+// @match        https://www.yandex.ru/*
 // @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
+// @match        https://psyholog.me/*
+// @match        https://motoreforma.com/*
 // @grant        none
 // ==/UserScript==
+let sites = {
+    "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai":["как звучит кларнет", "как звучит флейта"],
+    "psyholog.me":["центр здоровых отношений", "Услуги центра здоровых отношений", "Чекалина Елена психолог"],
+    "motoreforma.com":["мотореформа", "прошивки для CAN-AM", "тюнинг Maverikc X3", "тюнинг для квадроциклов CAN-AM", "вариатор CV-Tech для Can-Am"]
+}
 
-let keywords = ['как звучит кларнет', 'как звучит флейта'];
+let site = Object.keys(sites)[getRandom(0, Object.keys(sites).length)];//сайт
+let keywords = sites[site];
 let button = document.querySelector('.button_theme_search');
 let yandexInput =document.getElementsByName('text')[0];
 let links = document.links;
@@ -18,6 +26,14 @@ let keyword = keywords[getRandom(0,keywords.length)];
 //yandexInput.value = keyword;
 
 if(button!== null && button !== undefined){
+    document.cookie = `site=${site}`;
+}else if(location.hostname == "https://ya.ru/"){
+    site = getCookie("site");
+}else {
+    site = location.hostname;
+}
+if(button!== null && button !== undefined){
+    document.cookie = `site=${site}`;
     let i = 0;
     let timerId = setInterval(()=> {
         yandexInput.value += keyword[i];
@@ -25,22 +41,22 @@ if(button!== null && button !== undefined){
         if (i == keyword.length) {
             clearInterval(timerId);
             button.click();
-       }
+        }
     },500);
 
-}else if(location.hostname == 'xn----7sbab5aqcbiddtdj1e1g.xn--p1ai'){
+}else if(location.hostname == site){
     setInterval(()=>{
         let index = getRandom(0, links.length);
         if(getRandom(0,101)>=80){
-            location.href="https://ya.ru";
+            location.href="https://ya.ru/";
         }
-        if (links[index].href.indexOf("n----7sbab5aqcbiddtdj1e1g.xn--p1ai") !== -1)
+        if (links[index].href.indexOf(site) !== -1)
             links[index].click();}, getRandom(1000, 5000));
 
 } else{
-let nextYandexPage = true;
+    let nextYandexPage = true;
     for(let i=0; i<links.length; i++){
-        if (links[i].href.includes('xn----7sbab5aqcbiddtdj1e1g.xn--p1ai')){
+        if (links[i].href.includes(site)){
             let link = links[i];
             link.setAttribute("target", "_self"); //открытие в этом же окне
             let nextYandexPage = false;
@@ -49,9 +65,9 @@ let nextYandexPage = true;
             break;
         }
     }
-if (document.querySelector('[aria-label="Текущая страница 5"]')) {
+    if (document.querySelector('[aria-label="Текущая страница 5"]')) {
         let nextYandexPage = false;
-        location.href="https://ya.ru";
+        location.href="https://ya.ru/";
     }
     if (nextYandexPage) {
         setTimeout(()=>{document.querySelector('[aria-label="Следующая страница"]').click();},getRandom(2000,6000));
@@ -60,4 +76,10 @@ if (document.querySelector('[aria-label="Текущая страница 5"]')) 
 
 function getRandom(min, max){
     return Math.floor(Math.random()*(max-min)+min);
+}
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
 }
